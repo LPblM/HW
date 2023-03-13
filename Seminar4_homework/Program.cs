2289a0f73933a7b48a62ae240c7268b606ec0bfa
Sergey Kochkin <kochkins@gmail.com>
2023-03-13 17:36:42 +0300

    seminar4_homework done

--- Seminar4_homework/Program.cs
+++ Seminar4_homework/Program.cs
@@ -5,6 +5,37 @@
 
 // 2, 4 -> 16
 
+// int Exp (int a, int b)
+// {
+//     int count = 1;
+//     int exp = a;
+//     while(count <b)
+//     {
+//         exp = a * exp;
+//         count++;
+//     }
+//     return exp;
+// }
+
+// bool CheckB (int b)
+// {
+//     if ( b <= 0 )     
+//     {
+//         Console.WriteLine($"ERROR: Number B cannot have zero or negative value. Please try again."); 
+//         return false;
+//     }
+//     return true;
+// }
+
+// Console.WriteLine ("Insert number A:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Insert number B:");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (CheckB(b))
+// {
+//     Console.WriteLine($"Number {a} in power {b} is {Exp (a,b)}");
+// }
 
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
@@ -15,23 +46,22 @@
 
 // 9012 -> 12
 
-// void Palindrome(int rn)
+// void DigitsSum (int rn)
 // {
-//     int first = rn / 10000;
-//     int last = rn % 10;
-//     if (first == last)
-//     {
-//         int second = rn / 1000 % 10;
-//         int penultimate = rn / 10 % 10;
-//             if (second == penultimate)
-//             Console.WriteLine($"Number {rn} is the Palindrome");
+//     int sum = 0;
+//     int current = Math.Abs(rn);
+//     while (current > 0 )
+//     {     
+//         int rem = current % 10;
+//         sum = sum + rem ;
+//         current = current / 10;
 //     }
-//     else
-//     Console.WriteLine($"Number {rn} is NOT a Palindrome");
+//     Console.WriteLine($"Digits sum of {rn} number is {sum}");
 // }
-// int rn = new Random().Next(10000, 100000);
-// // int rn = 43534;
-// Palindrome(rn);
+// Console.WriteLine("Please insert the number");
+// int rn = Convert.ToInt32(Console.ReadLine());
+// // int rn = 9012;
+// DigitsSum(rn);
 
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
@@ -40,29 +70,44 @@
 
 // 6, 1, 33 -> [6, 1, 33]
 
-int [] CreateRandomArray (int size, int minValue, int maxValue)
-{
-    int [] newArray = new int [size];
-    for (int i = 0; i < size; i++)
-    {
-        newArray [i] = new Random().Next(minValue, maxValue + 1);
-    }
-    return newArray;
-}
+// int [] CreateRandomArray (int size, int minValue, int maxValue)
+// {
+//     int [] newArray = new int [size];
+//     for (int i=0; i < size; i++)
+//     {
+//         newArray [i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return newArray;
+// }
 
-void ShowArray (int [] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array [ i ] + " ");
-    }
-    Console.WriteLine ();
-}
-Console.WriteLine ("Insert array size:");
-int size = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine ("Insert minimum value:");
-int minValue = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine ("Insert maximum value:");
-int maxValue = Convert.ToInt32(Console.ReadLine());
+// void ShowArray (int [] array)
+// {
+//     Console.Write ("[");
+//     for(int i = 0; i < array.Length - 1; i++)
+//     {
+//         Console.Write($"{array [i]}, ");
+//     }
+//     Console.Write ($"{array[array.Length-1]}]");
+// }
 
-ShowArray (CreateRandomArray (size, minValue, maxValue));
+// bool CheckSize (int size)
+// {
+//     while ( size <= 0 )
+//     {
+//         Console.WriteLine($"Error: Array size {size} cannot have zero or negative value. Please try again.");
+//         return false;
+//     }
+//     return true;
+// }
+
+// Console.WriteLine ("Insert array size:");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Insert minimum value:");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Insert maximum value:");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+
+// if (CheckSize (size))
+// {
+//     ShowArray (CreateRandomArray (size, minValue, maxValue));
+// }
